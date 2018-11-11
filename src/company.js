4c77afc95a9e45ea0476c8e46e7197a2f5c94b83
Mbocharov <msbocharov@gmail.com>
2018-11-11 21:23:28 +0200

    extract array with two email from list with duplicates

--- src/company.js
+++ src/company.js
@@ -3,39 +3,33 @@ class Company {
     constructor() {
     }
     getEmailByHighestPosition(employees, limit) {
-        let resultEmails = [];
-        let employee = employees[0];
-        let i = 1;
-        if (limit === 1) {
-            while (employees.length > i) {
-                if (employees[i].position > employee.position) {
-                    employee = employees[i];
-                }
-                i += 1;
-            }
-            resultEmails.push(employee.email);
-        } else {
-            const employeeList = [];
-            employeeList[0] = employees[0];
-            employeeList[1] = employees[1];
-            while (employees.length > i) {
-                if (employees[i].position > employeeList[0].position) {
-                    employeeList[0] = employees[i];
-                } else if (employees[i].position > employeeList[1].position) {
-                    employeeList[1] = employees[i];
-                }
-                i += 1;
-            }
-            resultEmails = employeeList.map(item => item.email);
+        const employeeList = [];
+        for (let i = 0; i < limit; i += 1) {
+            employeeList.push(employees[i]);
         }
-        return resultEmails;
+        // employeeList.reverse();
+        let i = 1;
+        while (employees.length > i) {
+            const employee = employeeList.sort((a, b) => (b.position - a.position)).pop();
+            if (employees[i].position > employee.position) {
+                employeeList.push(employees[i]);
+            } else {
+                employeeList.push(employee);
+            }
+            i += 1;
+        }
+        return employeeList.map(item => item.email);
     }
 }
 
 const Position = {
     junior: 1,
     middle: 2,
-    senior: 3
+    senior: 3,
+    teamLead: 4,
+    projectManager: 5,
+    cto: 20
+
 };
 
 module.exports = {
--- test/company.test.js
+++ test/company.test.js
@@ -56,6 +56,7 @@ describe('Company', () => {
 
         expect(emails).to.have.members(['senior@company.com']);
     });
+
     it('extract two email when list contains three items', () => {
         const employees = [
             {
@@ -74,5 +75,33 @@ describe('Company', () => {
         const emails = company.getEmailByHighestPosition(employees, 2);
 
         expect(emails).to.have.members(['middle@company.com', 'senior@company.com']);
+    });
+
+    it('extract two email when list contains same items position', () => {
+        const employees = [
+            {
+                position: Position.junior,
+                email: 'junior1@company.com'
+            },
+            {
+                position: Position.junior,
+                email: 'junior2@company.com'
+            },
+            {
+                position: Position.middle,
+                email: 'middle@company.com'
+            },
+            {
+                position: Position.projectManager,
+                email: 'projectManager@company.com'
+            },
+            {
+                position: Position.cto,
+                email: 'cto@company.com'
+            }
+        ];
+        const emails = company.getEmailByHighestPosition(employees, 2);
+
+        expect(emails).to.have.members(['projectManager@company.com', 'cto@company.com']);
     });
 });
